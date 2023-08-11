94b6ceb1c443bfae94418b666957231c901ac824
Joaco-Latitude <joaquin.zanardi.t1vl@gmail.com>
2023-08-10 21:09:16 -0300

    cambios en el ingreso de alumnos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 const radioDni = document.querySelectorAll('.radio-dni');
 const radioAr = document.querySelectorAll('.radio-ar');
 const radioProv = document.querySelectorAll('.radio-prov');
+const radioLoc = document.querySelectorAll('.radio-loc');
 const dni_si = document.querySelector('.dni_si');
 const dni_no = document.querySelector('.dni_no');
 const arg = document.querySelector('.arg');
+const prov = document.querySelector('.prov');
+const otra = document.querySelector('.otra');
+const radioHerm = document.querySelectorAll('.radioHerm');
+const hermSi = document.querySelector('.hermSi');
+
+
 
 radioDni[0].onclick = () => {
 
@@ -42,9 +49,25 @@ radioAr[1].onclick = () => {
 
 radioProv[0].onclick = () => {
 
-    arg.classList.add('active');
+    prov.classList.add('active');
+    otra.classList.remove('active');
 }
 
+radioProv[1].onclick = () => {
+
+    otra.classList.add('active');
+    prov.classList.remove('active');
+}
+
+radioHerm[0].onclick = () => {
+
+    hermSi.classList.add('active');
+}
+
+radioHerm[1].onclick = () => {
+
+    hermSi.classList.remove('active');
+}
 
 
 

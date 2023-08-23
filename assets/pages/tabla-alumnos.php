0a0408746262a6dd8469edca146a4cf01f2bcc1c
indra <joaquin.zanardi.t1vl@gmail.com>
2023-08-23 15:16:06 -0300

    serchbar funciona

--- assets/pages/tabla-alumnos.php
+++ assets/pages/tabla-alumnos.php
@@ -1,12 +1,18 @@
 <?php
 include("../php/lib/conn.php");
-$query = "SELECT * FROM alumnos";
-$getQuery = mysqli_query($conn, $query);
 
-if ($getQuery) {
-    $alumnos = mysqli_fetch_all($getQuery, MYSQLI_ASSOC);
+if (isset($_GET['buscador'])) {
+    $buscador = $_GET['buscador'];
+    $consulta = "SELECT * FROM alumnos WHERE DNI LIKE '%{$buscador}%' OR DNIe LIKE '%{$buscador}%';";
+    $getQuery = mysqli_query($conn, $consulta);
+
+    if ($getQuery) {
+        $alumnos = mysqli_fetch_all($getQuery, MYSQLI_ASSOC);
+    } else {
+        echo "Error en la consulta: " . mysqli_error($conn);
+    }
 } else {
-    echo "Error en la consulta: " . mysqli_error($conn);
+    $alumnos = mysqli_query($conn, "SELECT * FROM alumnos");
 }
 
 ?>
@@ -22,12 +28,16 @@ if ($getQuery) {
 </head>
 
 <body>
+<form action="" method="get">
+        <input type="text" name="buscador" placeholder="Buscar Alumno por DNI"><br>
+        <input type="submit" name="enviar" value="Buscar">
+    </form>
 
     <?php if (!empty($alumnos)): ?>
         <div id="class-container">
             <?php foreach ($alumnos as $post) : ?>
                 <div>
-                <a href="alumno.php?idalumno=<?php echo $post["idAlumno"];?>">
+                    <a href="alumno.php?idalumno=<?php echo $post["idAlumno"];?>">
                         <h5><?= $post['apellidos'] . $post['nombres']?></h5>
                         <h5><?= $post['DNI'] . $post['DNIe']?></h5>
                         <button>Ver alumno</button>
@@ -38,16 +48,14 @@ if ($getQuery) {
     <?php else : ?>
         <p>No hay alumnos registrados.</p>
     <?php endif; ?>
-    <form action="" method="get">
-        <input type="text" name="buscador" placeholder="Buscar Alumno por DNI"><br>
-        <input type="submit" name="enviar" value="Buscar">
-    </form>
+
 
     <a href="../../index.html">Volver</a>
 
 </body>
 
 </html>
+
 
 
 

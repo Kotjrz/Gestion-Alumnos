1a8e559dd0b9c9f6fd5616e0a44638de556f102e
Joaco-Latitude <joaquin.zanardi.t1vl@gmail.com>
2023-09-13 19:29:53 -0300

    pagination agregado, quedan por cambiar los estilos

--- assets/pages/tabla-alumnos.php
+++ assets/pages/tabla-alumnos.php
@@ -1,130 +1,91 @@
 <?php
+
 include("../php/lib/conn.php");
 
-/* if (isset($_GET['buscador'])) {
-    $buscador = $_GET['buscador'];
-    $consulta = "SELECT * FROM alumnos WHERE DNI LIKE '%{$buscador}%' OR DNIe LIKE '%{$buscador}%';";
-    $getQuery = mysqli_query($conn, $consulta);
+// Define la pagina actual
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
 
-    if ($getQuery) {
-        $alumnos = mysqli_fetch_all($getQuery, MYSQLI_ASSOC);
-    } else {
-        echo "Error en la consulta: " . mysqli_error($conn);
-    }
-} else {
-    $alumnos = mysqli_query($conn, "SELECT * FROM alumnos");
-}
+//Cantidad de elementos por pagina
+$limit = 5;
 
-$resCurso = mysqli_query($conn, "SELECT * from alumnos") */
-/* if (!isset($_POST['buscar'])) {
-    $_POST['buscar'] = '';
-}
-if (!isset($_POST['buscacurso'])) {
-    $_POST['buscacurso'] = '';
-}
-if (!isset($_POST['orden'])) {
-    $_POST['orden'] = '';
-    }
- */
+// calcula el offset
+$offset = ($page - 1) * $limit;
 
-if(isset($_GET['search'])){
-    $searchQuery = $_GET['search'];
-    $consulta = "SELECT * FROM alumnos WHERE DNI LIKE '%{$searchQuery}%' OR DNIe LIKE '%{$searchQuery}%';";
-    $getQuery = mysqli_query($conn, $consulta);
+// usando el limite y el offset calcula la consulta
+$consultaPaginacion = "SELECT * FROM alumnos LIMIT $limit OFFSET $offset";
+$getQueryPaginacion = mysqli_query($conn, $consultaPaginacion);
 
-    if ($getQuery) {
-        $alumnos = mysqli_fetch_all($getQuery, MYSQLI_ASSOC);
-    }
+// Cantidad deintems
+$consultaTotal = "SELECT COUNT(*) as total FROM alumnos";
+$getQueryTotal = mysqli_query($conn, $consultaTotal);
+$rowTotal = mysqli_fetch_assoc($getQueryTotal);
+$totalItems = $rowTotal['total'];
 
-    else {
-        echo "Error in the search query: " . mysqli_error($conn);
-    }
-}
+// cantidad de paginas
+$totalPages = ceil($totalItems / $limit);
+
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alumnos</title>
+    <title>Tabla alumnos</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../css/style.css">
+    </style>
 </head>
 
 <body>
-
-    <h1>Alumnos</h1>
-
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-        <input type="text" name="search" placeholder="Buscar alumno">
-        <input type="submit" value="Buscar">
-    </form>
-
-    <div>
-        <table border="1">
+    <table class="table" border="1">
+        <thead>
             <tr>
-                <td>DNI</td>
-                <td>Apellido</td>
-                <td>Nombre</td>
-                <td>Curso</td>
-                <td>Division</td>
-                <td>Detalle</td>
+                <th>DNI</th>
+                <th>Apellidos</th>
+                <th>Nombres</th>
+                <th>Column 4</th>
+                <th>Column 5</th>
+                <th>Detalles</th>
             </tr>
-            <tr>
-                <?php
-                foreach ($alumnos as $rowSql) {
-                ?>
-            <tr>
-                <td><?php echo $rowSql['DNI'] ?></td>
-                <td><?php echo $rowSql['apellidos'] ?></td>
-                <td><?php echo $rowSql['nombres'] ?></td>
-                <td>No hecho</td>
-                <td>No hecho</td>
-                <td><a href="alumno.php?idalumno=<?php echo $rowSql["idAlumno"]; ?>" class="a-detalles">Detalles</a></td>
-            </tr>
-        <?php
-                }
+        </thead>
+        <tbody>
+            <?php while ($rowSql = mysqli_fetch_assoc($getQueryPaginacion)) { ?>
+                <tr>
+                    <td><?php echo $rowSql['DNI']; ?></td>
+                    <td><?php echo $rowSql['apellidos']; ?></td>
+                    <td><?php echo $rowSql['nombres']; ?></td>
+                    <td>No hecho</td>
+                    <td>No hecho</td>
+                    <td><a href="alumno.php?idalumno=<?php echo $rowSql["idAlumno"]; ?>" class="a-detalles">Detalles</a></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 
-        ?>
-        </tr>
+    <div class="pagination">
+        <?php if ($page > 1) { ?>
+            <a href="?page=<?php echo ($page - 1); ?>">Previous</a>
+        <?php } ?>
 
-        </table>
+        <?php for ($i = 1; $i <= $totalPages; $i++) { ?>
+            <a href="?page=<?php echo $i; ?>" <?php if ($i == $page) {
+                                                    echo 'class="active"';
+                                                } ?>><?php echo $i; ?></a>
+        <?php } ?>
+
+        <?php if ($page < $totalPages) { ?>
+            <a href="?page=<?php echo ($page + 1); ?>">Next</a>
+        <?php } ?>
     </div>
-
-    <!--   ESTRUCTURA DE ALUMNOS CON TIPO CARDS DE RED SOCIAL
-    <?php if (!empty($alumnos)) : ?>
-        <div id="class-container">
-            <?php foreach ($alumnos as $post) : ?>
-                <div>
-                    <a href="alumno.php?idalumno=<?php echo $post["idAlumno"]; ?>">
-                        <h5><?= $post['apellidos'] . $post['nombres'] ?></h5>
-                        <h5><?= $post['DNI'] . $post['DNIe'] ?></h5>
-                        <button>Ver alumno</button>
-                    </a>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    <?php else : ?>
-        <p>No hay alumnos registrados.</p>
-    <?php endif; ?> -->
-
-
-    <a href="../../index.html">Volver</a>
+    <div>
+        <a href="../../index.html">Volver</a>
+    </div>
 
 </body>
 
 </html>
 
+<?php
+mysqli_close($conn);
 
-
-
-<!-- if(isset($_GET['enviar']))
-{
-  $busqueda = $_GET['buscador'];
-  $consulta = $conn->query("SELECT * FROM alumnos WHERE DNI LIKE '%{$busqueda}%' OR DNIe LIKE '%{$busqueda}%';");
-
-  while($row = $consulta ->fetch_array())
-  {
-    echo $row['DNI'] . $row['DNIe'] . '<br>';
-  } -->
+?>

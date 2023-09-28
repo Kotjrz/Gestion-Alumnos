60cb4191b304dc3c9ebaafb4bd2001fa9a9b8684
Joaco-Latitude <joaquin.zanardi.t1vl@gmail.com>
2023-09-27 21:27:40 -0300

    Funciona el edit de datos

--- assets/pages/alumno.php
+++ assets/pages/alumno.php
@@ -1,94 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Campo</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
+
 <body>
     <div class="wrapper">
-    <?php
-    // Incluye el archivo de conexión a la base de datos.
-    include("../php/lib/conn.php");
+        <?php
+        // Incluye el archivo de conexión a la base de datos.
+        include("../php/lib/conn.php");
 
-    // Obtiene el ID del alumno desde la URL usando el método GET.
-    $id_alumno = $_GET['idalumno'];
+        // Obtiene el ID del alumno desde la URL usando el método GET.
+        $id_alumno = $_GET['idalumno'];
 
-    try {
-        // Prepara una consulta SQL utilizando una sentencia preparada para evitar inyecciones SQL.
-        $stmt = $conn->prepare("SELECT * FROM alumnos WHERE idAlumno = ?");
-        $stmt->bind_param("i", $id_alumno); // "i" indica que se espera un valor entero.
-        $stmt->execute(); // Ejecuta la consulta preparada.
-        $result = $stmt->get_result(); // Obtiene el resultado de la consulta.
+        try {
+            // Prepara una consulta SQL utilizando una sentencia preparada para evitar inyecciones SQL.
+            $stmt = $conn->prepare("SELECT * FROM alumnos WHERE idAlumno = ?");
+            $stmt->bind_param("i", $id_alumno); // "i" indica que se espera un valor entero.
+            $stmt->execute(); // Ejecuta la consulta preparada.
+            $result = $stmt->get_result(); // Obtiene el resultado de la consulta.
 
-        // Verifica si la consulta devolvió al menos una fila de resultados.
-        if ($result->num_rows > 0) {
-            // Si hay resultados, obtiene la primera fila de resultados.
-            $row = $result->fetch_assoc();
+            // Verifica si la consulta devolvió al menos una fila de resultados.
+            if ($result->num_rows > 0) {
+                // Si hay resultados, obtiene la primera fila de resultados.
+                $row = $result->fetch_assoc();
 
-            // Define un mapeo de nombres de columnas a nombres más legibles.
-            $columnNamesMap = [
-                'fechaDeNacimiento' => 'Fecha de nacimiento',
-                'tipoDoc' => 'Tipo de documento',
-                'numeroDoc' => 'Número de documento',
-                'nombre' => 'Nombre',
-                'apellido' => 'Apellido',
-                'direccion' => 'Dirección actual',
-                'telefono' => 'Teléfono',
-                'documentoExtranjero' => 'Documento Extrajero',
-                'idenGenero' => 'Identidad de Genero',
-                'telefonoFijo' => 'Teléfono Fijo',
-                'telefonoCelular' => 'Teléfono Celular',
-                'cantHermanos' => 'Cantidad de hermanos',
-                'hermEscuela' => 'Hermanos en la escuela',
-                'provDomicilio' => 'Provincia de domicilio actual',
-                'provincia' => 'Provincia de nacimiento',
-                'nacionalidad' => 'Nacionalidad',
-                'DNIe' => 'DNI Extrajero',
-                'distriDomicilio' => 'Distrito de domicilio actual',
-                'localiDomicilio' => 'Localidad de domicilio actual',
-                'distrito' => 'Distrito de nacimiento',
-                'localidad' => 'Localidad de nacimiento',
+                // Define un mapeo de nombres de columnas a nombres más legibles.
+                $columnNamesMap = [
+                    'fechaDeNacimiento' => 'Fecha de nacimiento',
+                    'tipoDoc' => 'Tipo de documento',
+                    'numeroDoc' => 'Número de documento',
+                    'nombre' => 'Nombre',
+                    'apellido' => 'Apellido',
+                    'direccion' => 'Dirección actual',
+                    'telefono' => 'Teléfono',
+                    'documentoExtranjero' => 'Documento Extrajero',
+                    'idenGenero' => 'Identidad de Genero',
+                    'telefonoFijo' => 'Teléfono Fijo',
+                    'telefonoCelular' => 'Teléfono Celular',
+                    'cantHermanos' => 'Cantidad de hermanos',
+                    'hermEscuela' => 'Hermanos en la escuela',
+                    'provDomicilio' => 'Provincia de domicilio actual',
+                    'provincia' => 'Provincia de nacimiento',
+                    'nacionalidad' => 'Nacionalidad',
+                    'DNIe' => 'DNI Extrajero',
+                    'distriDomicilio' => 'Distrito de domicilio actual',
+                    'localiDomicilio' => 'Localidad de domicilio actual',
+                    'distrito' => 'Distrito de nacimiento',
+                    'localidad' => 'Localidad de nacimiento',
+                ];
+
+                // Muestra los datos del estudiante.
+                echo '<h2>Datos del Estudiante</h2>';
+
+        ?>
+
+                <div>
+
+                    <?php
+
+                    foreach ($row as $columnName => $value) {
+                        // Verifica si hay un nombre de columna legible en el mapeo.
+                        $displayColumnName = isset($columnNamesMap[$columnName]) ? $columnNamesMap[$columnName] : $columnName;
 
 
-            ];
 
-            // Muestra los datos del estudiante.
-            echo '<h2>Datos del Estudiante</h2>';
+                        // Muestra la información del estudiante.
+                        echo '<p><strong>' . $displayColumnName . ':</strong> ' . $value . '</p>';
+                    }
+                    ?>
 
-            foreach ($row as $columnName => $value) {
-                // Verifica si hay un nombre de columna legible en el mapeo.
-                $displayColumnName = isset($columnNamesMap[$columnName]) ? $columnNamesMap[$columnName] : $columnName;
-                
-                // Muestra la información del estudiante.
-                echo '<p><strong>' . $displayColumnName . ':</strong> ' . $value . '</p>';
+                </div>
+
+        <?php
+
+                // Agrega un enlace para editar cada campo individualmente.
+                echo '<h2>Editar Campos</h2>';
+
+                foreach ($row as $columnName => $value) {
+                    $editLink = '../php/editar-campo.php?column=' . $columnName . '&idalumno=' . $id_alumno;
+                    $displayColumnName = isset($columnNamesMap[$columnName]) ? $columnNamesMap[$columnName] : $columnName;
+                    echo '<p><a href="' . $editLink . '">Editar ' . $displayColumnName . '</a></p>';
+                }
+
+                // Agrega un enlace para volver a la página de tabla de alumnos.
+                echo '<p><a href="../pages/tabla-alumnos.php">Volver</a></p>';
+            } else {
+                // Si no se encontraron resultados, muestra un mensaje de error.
+                echo "No se encontró ningún alumno con el ID proporcionado.";
             }
 
-            // Agrega un enlace para editar cada campo individualmente.
-            echo '<h2>Editar Campos</h2>';
-
-            foreach ($row as $columnName => $value) {
-                $editLink = '../php/editar-campo.php?column=' . $columnName . '&idalumno=' . $id_alumno;
-                $displayColumnName = isset($columnNamesMap[$columnName]) ? $columnNamesMap[$columnName] : $columnName;
-                echo '<p><a href="' . $editLink . '">Editar ' . $displayColumnName . '</a></p>';
-            }
-
-            // Agrega un enlace para volver a la página de tabla de alumnos.
-            echo '<p><a href="../pages/tabla-alumnos.php">Volver</a></p>';
-        } else {
-            // Si no se encontraron resultados, muestra un mensaje de error.
-            echo "No se encontró ningún alumno con el ID proporcionado.";
+            // Cierra la sentencia preparada y la conexión a la base de datos.
+            $stmt->close();
+            $conn->close();
+        } catch (Exception $e) {
+            // Muestra un mensaje de error detallado en caso de excepción.
+            echo "Error: " . $e->getMessage();
         }
-
-        // Cierra la sentencia preparada y la conexión a la base de datos.
-        $stmt->close();
-        $conn->close();
-    } catch (Exception $e) {
-        // Muestra un mensaje de error detallado en caso de excepción.
-        echo "Error: " . $e->getMessage();
-    }
-    ?>
+        ?>
     </div>
 </body>
+
 </html>
--- assets/php/edit-alumno.php
+++ assets/php/edit-alumno.php
@@ -1,39 +1,59 @@
-<?php
-// Incluye el archivo de conexión a la base de datos.
-include("../php/lib/conn.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Campo</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+    <?php
+    // Incluye el archivo de conexión a la base de datos.
+    include("../php/lib/conn.php");
 
-// Obtiene el ID del alumno desde la URL usando el método GET.
-$id_alumno = isset($_GET['idalumno']) ? $_GET['idalumno'] : null;
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtener los valores actualizados del formulario
-    $column = $_POST['column'];
-    $value = $_POST['new_value'];
-
-    print_r($_POST);
+    // Obtiene el ID del alumno y la columna desde el formulario.
+    $id_alumno = isset($_POST['idalumno']) ? $_POST['idalumno'] : null;
+    $column = isset($_POST['column']) ? $_POST['column'] : null;
 
     try {
-        // Prepara una consulta SQL utilizando una sentencia preparada
-        $stmt = $conn->prepare("UPDATE alumnos SET $column = ? WHERE idAlumno = ?");
-        $stmt->bind_param("ss", $column, $value);; // "si" indica que se espera un valor string y un valor entero.
-        $stmt->execute(); // Ejecuta la consulta preparada.
+        // Verifica si se proporcionaron valores válidos.
+        if ($id_alumno !== null && $column !== null) {
+            // Obtiene el nuevo valor desde el formulario.
+            $new_value = isset($_POST['new_value']) ? $_POST['new_value'] : null;
 
-        // Verifica si se actualizó al menos una fila.
-        if ($stmt->affected_rows > 0) {
-            echo "Registro actualizado correctamente.";
+            // Verifica si se proporcionó un nuevo valor.
+            if ($new_value !== null) {
+                // Prepara una consulta SQL utilizando una sentencia preparada para actualizar el campo seleccionado.
+                $stmt = $conn->prepare("UPDATE alumnos SET $column = ? WHERE idAlumno = ?");
+                $stmt->bind_param("si", $new_value, $id_alumno); // "si" indica que se espera un valor string y un valor entero.
+
+                // Ejecuta la consulta preparada.
+                if ($stmt->execute()) {
+                    echo "Campo actualizado correctamente.";
+                } else {
+                    echo "Error al actualizar el campo.";
+                }
+
+                // Cierra la sentencia preparada.
+                $stmt->close();
+            } else {
+                echo "No se proporcionó un nuevo valor.";
+            }
         } else {
-            echo "No se encontró ningún registro para actualizar.";
+            echo "No se proporcionó un ID de alumno o una columna válida.";
         }
 
-        // Cierra la sentencia preparada.
-        $stmt->close();
+        // Agrega un enlace para volver a la página de detalles del alumno.
+        echo '<p><a href="../pages/alumno.php?idalumno=' . $id_alumno . '">Volver a Detalles</a></p>';
+
+        // Cierra la conexión a la base de datos.
+        $conn->close();
     } catch (Exception $e) {
         // Muestra un mensaje de error detallado en caso de excepción.
         echo "Error: " . $e->getMessage();
     }
-}
-
-// Cierra la conexión a la base de datos.
-$conn->close();
-?>
+    ?>
+    </div>
+</body>
+</html>

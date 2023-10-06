84ce6c576655cf0f540d622d70ebf6ebd231aed7
dantedii <dante.diiorio@gmail.com>
2023-10-06 19:53:46 -0300

    curso agregado

---
+++ assets/api/cursos.php
@@ -0,0 +1,39 @@
+<?php
+
+include('../php/lib/conn.php');
+
+header("Content-Type: application/json");
+
+// Verificar si se recibió una solicitud GET con el parámetro DNI
+if(isset($_GET['alumnoID'])) {
+    $alumno = $_GET['alumno'];
+    // Prevenir inyección SQL usando mysqli_real_escape_string o consultas preparadas
+    $alumno = mysqli_real_escape_string($conn, $alumno);
+
+    $consulta = mysqli_query($conn, "SELECT * FROM curso WHERE ID_curso in (SELECT Curso_CA FROM curso_alumnos WHERE Alumnos_CA = $alumno); ");
+
+    $result = $consulta->fetch_all();
+
+    echo json_encode($result);
+} else if(isset($_GET['alumnoDNI'])) {
+    $dni = $_GET['alumnoDNI'];
+    // Prevenir inyección SQL usando mysqli_real_escape_string o consultas preparadas
+    $dni = mysqli_real_escape_string($conn, $dni);
+
+    $consulta = mysqli_query($conn, "SELECT * FROM curso WHERE ID_curso in 
+                                    (SELECT Curso_CA FROM curso_alumnos WHERE Alumnos_CA in 
+                                    (SELECT idAlumno from alumnos where DNI = '$dni')); ");
+
+    $result = $consulta->fetch_all();
+
+    echo json_encode($result);
+} else {
+
+    $consulta = mysqli_query($conn, "SELECT * FROM materia");
+
+    $result = $consulta->fetch_all();
+
+    echo json_encode($result);
+
+}
+?>
